5a29ba3f7c60180f9180df382024353ae247288e
skotlo <skotlo@stu.naperville203.org>
2023-02-02 11:50:43 -0600

    working on room number validation

--- Web-App/app/main/forms.py
+++ Web-App/app/main/forms.py
@@ -1,6 +1,6 @@
 from flask_wtf import FlaskForm
 from wtforms import StringField, PasswordField, BooleanField, SubmitField, SelectField, SelectMultipleField, TextAreaField, widgets, RadioField, FieldList, FormField, IntegerField
-from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, InputRequired
+from wtforms.validators import DataRequired, ValidationError, Email, EqualTo, InputRequired, NumberRange
 from app.models import User, Flavor, MenuItem, Drink, Order, Temp, RoomNum, DrinksToFlavor
 
 class LoginForm(FlaskForm):
@@ -89,15 +89,11 @@ class FavoriteDrinksForm(FlaskForm):
 class OrderForm(FlaskForm):
     
     #room = SelectField(u'Room Number:', coerce=int, validators=[DataRequired()])
-    room = IntegerField('Room Number:', validators=[InputRequired()])
 
-
+    room = IntegerField('Room Number:', validators=[InputRequired(), NumberRange(min=100, max=292, message='Please enter room')])
+        
     submit = SubmitField('Order')
 
-    #def __init__(self):
-        #super(OrderForm, self).__init__()
-
-        #self.room.choices = [(r.id, r.num) for r in RoomNum.query.order_by(RoomNum.id)]
 
 class BaristaForm(FlaskForm):
 
