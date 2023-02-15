55f87521e7131d6100bea8cddbc94371ae67dbb9
nuggets4 <lzhang2@stu.naperville203.org>
2023-02-15 11:43:23 -0600

    made edits to add more info to myOrder

--- Web-App/app/main/routes.py
+++ Web-App/app/main/routes.py
@@ -202,7 +202,7 @@ def myOrder(orderId):
                 new_order_id = Order.query.all()[-1].id
                 current_user.current_order_id=new_order_id
                 db.session.commit()
-                return redirect(url_for('main.justOrdered', orderId=orderId)) #previously return redirect(url_for('main.myOrder', orderId=current_user.current_order_id))
+                return redirect(url_for('main.justOrdered', orderId=orderId)) 
         elif halfcaf.acc_order == False:
                 flash("This is not a time for ordering drinks ")
                 return redirect(url_for('main.home'))
